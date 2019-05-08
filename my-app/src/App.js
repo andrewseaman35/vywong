084fa0eb1b594b3a063310b21aa2adfb2d1e9be9
Andrew Seaman <aseaman@23andme.com>
2019-05-08 11:07:55 -0700

    Add router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import logo from './logo.svg';
+import Home from './views/Home';
+import Page from './views/Page';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path='/' render={() => (
+            <div className="App">
+              <Home />
+            </div>
+          )}/>
+          <Route exact={true} path='/page' render={() => (
+            <div className="App">
+              <Page />
+            </div>
+          )}/>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
---
+++ my-app/src/views/Page.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class Page extends Component {
+  state = {
+  }
+
+  render () {
+      return (
+        <div>
+           <h1> Page </h1>
+        </div>
+      )
+   }
+}
