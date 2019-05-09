b42b235cf195a7ef4a8059e70cab177d73880ba9
Andrew Seaman <aseaman@23andme.com>
2019-05-09 08:28:25 -0700

    Add homepage header

--- my-app/src/views/Home.js
+++ my-app/src/views/Home.js
@@ -9,22 +9,15 @@ export default class Home extends Component {
         return (
             <div>
                 <Header />
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
+                <div className="inner">
+                    <div className='homepage'>
+                        <h1 className='homepage-header'><strong>Hello there!</strong></h1>
+                        <p class="body">I am Victoria. I am a passionate, driven designer who loves:</p>
+                        <blockquote class="quote">
+                            <strong class="bold-text-2">Designing with impact<br/>Advocating for user needs<br/>Believing my cat loves me back<br/>Saving the best bite of food for last</strong>
+                        </blockquote>
+                    </div>
+                </div>
                 <Footer />
             </div>
         )
