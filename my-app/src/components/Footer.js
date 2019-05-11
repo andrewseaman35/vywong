17a229f6cb1fa36bcb26ec8d4c87c6ba796a87ec
Andrew Seaman <aseaman@23andme.com>
2019-05-11 14:56:49 -0700

    update footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
+
+const FOOTER_LINKS = [
+    [
+        {text: 'about me', href: '/about'},
+    ], [
+        {text: 'mycite', href: '/mycite'},
+        {text: 'white label', href: '/ybvr_white_label'},
+        {text: 'ybvr brand', href: '/ybvr_brand'},
+    ], [
+        {text: 'traverse',  href: '/traverse'},
+        {text: 'amma', href: '/amma'},
+        {text: 'other work', href: '/other'},
+    ],
+];
+
 export default class Footer extends Component {
   state = {
+  }
+
+  renderLinks(links) {
+    const linkElements = [];
+    links.forEach(function(link, i) {
+        linkElements.push(
+            <div key={i}><a className="footer-link" href={link.href}>{link.text}</a></div>
+        );
+    });
+    return linkElements;
+  }
+
+  renderLinkColumns() {
+    const linkColumns = [];
+    FOOTER_LINKS.forEach((function(linkColumn, i) {
+        linkColumns.push(
+            <div className="link-column" key={i}>
+                {this.renderLinks(linkColumn)}
+            </div>
+        );
+    }).bind(this));
+    return (
+        <div className="link-container">
+            {linkColumns}
+        </div>
+    )
   }
 
   render () {
@@ -11,19 +52,7 @@ export default class Footer extends Component {
                     <h6 className="footer-title">smile often.</h6>
                 </div>
                 <div className="link-container">
-                    <div className="link-column">
-                        <div><a className="footer-link">about me</a></div>
-                    </div>
-                    <div className="link-column">
-                        <div><a className="footer-link">mycite</a></div>
-                        <div><a className="footer-link">white label</a></div>
-                        <div><a className="footer-link">ybvr brand</a></div>
-                    </div>
-                    <div className="link-column">
-                        <div><a className="footer-link">traverse</a></div>
-                        <div><a className="footer-link">amma</a></div>
-                        <div><a className="footer-link">other work</a></div>
-                    </div>
+                    {this.renderLinkColumns()}
                 </div>
             </div>
         </div>
