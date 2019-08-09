71bfb896a53ad77e3727c99c5e9d5979fbe613ae
Victoria Wong <=>
2019-08-08 17:10:53 -0700

    Last commit, ready to merge!

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -8,6 +8,7 @@ export default class Hero extends Component {
             <div className="hero-container">
                 <div className={`hero ${this.props.project}`}>
                     <div className="hero-overlay"></div>
+                    //You're my hero and I love you! 
                 </div>
             </div>
         )
